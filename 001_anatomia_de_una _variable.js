8caa211a130f552d7d630e147bd0c666ed8a3760
Juan_Sierra <juandanieljdsda10@gmail.com>
2024-10-01 17:17:59 -0500

    Anatomia de una variable y declaracion de variables

---
+++ 001_anatomia_de_una _variable.js
@@ -0,0 +1,48 @@
+
+// declaracion de variables
+
+const equipo = "colombia";
+
+let portero = "David Ospina";
+
+let lateral_derecho = "Daniel Muñuz";
+let defensa_central_1 = "Yerry Mina";
+let defensa_central_2 = "Davinson Sanchez";
+let lateral_izquierdo = "Johan Mojica";
+
+let medio_centro_defensivo = "Jefferson Lerma";
+let medio_centro_ofensivo = "James Rodriguez";
+let medio_centro_mixto = "Matheus Uribe";
+
+let extremo_izquierdo = "Luis Diaz";
+let delantero_centro = "Rafel Santos";
+let extremo_derecho = "Luis Sinisterra";
+
+
+
+// Mostrar la alineación en la consola
+
+console.log("Equipo:", equipo);
+
+console.log("Portero:", portero);
+
+console.log("Defensas:");
+console.log("Lateral derecho:", lateral_derecho);
+console.log("Defensa central 1:", defensa_central_1);
+console.log("Defensa central 2:", defensa_central_2);
+console.log("Lateral izquierdo:", lateral_izquierdo);
+
+console.log("Mediocampistas:");
+console.log("Mediocentro defensivo:", medio_centro_defensivo);
+console.log("Mediocentro ofensivo:", medio_centro_ofensivo);
+console.log("Mediocentro mixto:", medio_centro_mixto);
+
+console.log("Delanteros:");
+console.log("Extremo izquierdo:", extremo_izquierdo);
+console.log("Delantero centro:", delantero_centro);
+console.log("Extremo derecho:", extremo_derecho);
+
+
+
+
+
