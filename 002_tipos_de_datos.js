cb603544b1b95d7c78016f8dd558d2dbd2157eb7
Juan_Sierra <juandanieljdsda10@gmail.com>
2024-10-01 17:51:21 -0500

    Tipos de datos en JS

---
+++ 002_tipos_de_datos.js
@@ -0,0 +1,31 @@
+//primitivos
+
+let nombre = "Juan" //string;
+
+let edad = 26; //number;
+
+let esMayorDeEdad = true; //boolean
+
+let noHayValor = null; // null
+
+let noDefinido = undefined; //undefined
+
+let simboloUnico = Symbol('unico');//symbol
+
+let numeroGrande = 2n; //bigint
+
+//Complejos 
+
+//Object
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Models S' 
+};
+
+// array
+let frutas = ['manzana', 'banana', 'uvas'];
+
+//function
+function saludar(){
+    
+}
